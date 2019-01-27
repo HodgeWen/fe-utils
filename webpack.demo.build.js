7edd094aed1fbe329d0e55d042311be47ab3a93d
whj <emailwen@qq.com>
2019-01-27 20:01:51 +0800

    update

--- webpack.demo.build.js
+++ webpack.demo.build.js
@@ -7,7 +7,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'demo'),
+        include: path.resolve(__dirname, 'demo'), // 只打包demo下的
         use: {
           loader: 'babel-loader',
           options: {
