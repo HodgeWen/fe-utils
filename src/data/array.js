8a6f17c05ddfbdf823ffeb7ca61252e4ce2f68c7
whj <emailwen@qq.com>
2019-02-25 11:07:43 +0800

    uopdTE

--- src/data/array.js
+++ src/data/array.js
@@ -1,45 +1,42 @@
-import {each} from '../common'
+import { each } from '../common'
 
 function add(...args) {
-  const ctx = this.data !== undefined ? this.data : this
-  return ctx.concat(args)
+	const ctx = this.data !== undefined ? this.data : this
+	return ctx.concat(args)
 }
 
 function minus(arr) {
-  const ctx = this.data !== undefined ? this.data : this
-  
+	const ctx = this.data !== undefined ? this.data : this
 }
 
 function set(id) {
-  const ctx = this.data !== undefined ? this.data : this
+	const ctx = this.data !== undefined ? this.data : this
 
-  if (Array.from && id === undefined) {
-    return Array.from(new Set(ctx))
-  }
+	if (Array.from && id === undefined) {
+		return Array.from(new Set(ctx))
+	}
 
-  const obj = Object.create(null)
-  const arr = []
-  if (id === undefined) {
-    each(ctx, v => {
-      const key = typeof v + v
-      if (!obj[key]) { 
-        obj[key] = true
-        arr.push(v)
-      }
-    })
-    return arr
-  }
-  each(ctx, v => {
-    const key = v[id]
-    if (!obj[key]) {
-      obj[key] = true
-      arr.push(v)
-    }
-  })
-  return arr
+	const obj = Object.create(null)
+	const arr = []
+	if (id === undefined) {
+		each(ctx, (v) => {
+			const key = typeof v + v
+			if (!obj[key]) {
+				obj[key] = true
+				arr.push(v)
+			}
+		})
+		return arr
+	}
+	each(ctx, (v) => {
+		const key = v[id]
+		if (!obj[key]) {
+			obj[key] = true
+			arr.push(v)
+		}
+	})
+	return arr
 }
-
-
 
 add.key = 'add'
 minus.key = 'minus'
