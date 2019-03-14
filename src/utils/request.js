4bd63dc15c4d4305c5075613059b4fdc4d97e9e6
wen <emailwen@qq.com>
2019-03-14 18:09:03 +0800

    udoater

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
-import Pro from './promise'
+import Ps from './promise'
 import { merge, serialize } from '../data/object.js'
 
+const Pro = window.Promise || Ps // 初始化兼容
 const data_tactics = {
 	json: 'application/json; charset=utf-8',
 	xml: 'application/x-www-form-urlencoded; charset=utf-8',
@@ -8,6 +9,97 @@ const data_tactics = {
   html: 'html/plain; charset=utf-8',
   form: 'multipart/form-data; charset=utf-8'
 }
+
+function createInstance (url, method, data, callback) {
+	
+}
+
+// 创建一个基于XMLHttpRequest的请求客户端
+function createReq ({
+	url, method = 'GET', data, timeout = 18000, success = function () {},
+	failed = function () {}
+} = {}) {
+	const client = new XMLHttpRequest()
+	const reg = /^(get|head)$/i
+	const isNotGetOrHead = !reg.test(method)
+	const serializeData = serialize.call(data)
+
+	if (isNotGetOrHead) {
+		data = serializeData
+	} else {
+		url = url + '?' + serializeData
+		data = null
+	}
+
+	client.onreadystatechange = function () {
+		if (this.readyState !== 4) return false 	
+		if (this.status >= 400 && this.status < 600 ) {
+			failed(this.status, this.statusText)
+		}
+
+		success(this)
+	}
+
+	client.open(method, url, true)
+	client.timeout = timeout
+	client.ontimeout = function () {
+		failed(this.status)
+	}
+
+	client.send(data)
+}
+
+// 
+function createFetch () {
+	return fetch
+}
+
+class Request {
+
+	constructor () {
+		this.baseUrl = ''
+		this.timeout = 18000
+	}
+
+	request () {
+		
+
+		return new Pro((resolve, reject) => {
+
+		})
+	}
+
+	get (url, data) {
+		return this.request({
+			method: 'get', url, data
+		})
+	}
+
+	post (url, data) {
+		return this.request({
+			method: 'POST', url, data
+		})
+	}
+
+	delete () {
+		return this.request({
+			method: 'DELETE', url, data
+		})
+	}
+
+	put () {
+		return this.request({
+			method: 'PUT', url, data
+		})
+	}
+
+	patch () {
+		return this.request({
+			method: 'PATCH', url, data
+		})
+	}
+}
+
 
 function request({ 
   url = '', 
