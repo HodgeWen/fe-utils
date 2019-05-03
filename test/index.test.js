23f00e6d0a55e52d9486cf10c1b523598ef00b97
whj <emailwen@qq.com>
2019-05-03 12:28:03 +0800

    update

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,19 @@
-import { wt, each, getType } from '../src/index'
-import { $, hasClass } from '../src/index'
+import {$, wt, utils} from '../src'
 
 describe('测试wt的原始功能', () => {
   
   test('获取属性', () => {
+    const inst = [1, '1', true, {}, []]
+    expect(inst.map(item => wt(item).type)).toEqual(['Number', 'String', 'Boolean', 'Object', 'Array'])
     expect(wt(3).type).toBe('Number')
   })
 
   test('遍历', () => {
-    const ret = wt(3).each(() => {})
-    let n = 0
-    each.call(3, () => n++)
-    expect(ret).toBe(true)
-    expect(n).toBe(3)
-  })
-})
-
-
-describe('测试$的原始功能', () => {
-  test('获取节点', () => {
-    expect($().hasClass()).toBe('Array')
+    const arr = [1, 2, 3, 4, 5]
+    const ret = []
+    wt(arr).each(item => ret.push(item + 1))
+    expect(ret).toEqual([2, 3, 4, 5, 6])
+    
   })
 })
 
