f42ec9fa5f0355dad5eb61ac9c98febcf4f060a0
wen <emailwen@qq.com>
2019-05-16 18:02:40 +0800

    update

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -4,23 +4,23 @@
 // 提供then方法，在pendding阶段不会被调用 接收两个参数 onFulfilled, onRejected
 // 4 then方法返回一个新的Promise对象, 并且resolve上一个promise对象的回调返回值
 
-const resolve = ctx => {
-  return value => {
-    ctx.value = value
-    ctx.status = 'resolved'
-    if (!ctx.thenMethod) return
-    ctx.value = ctx.thenExcute(this.value)
-  }
-}
+// const resolve = ctx => {
+//   return value => {
+//     ctx.value = value
+//     ctx.status = 'resolved'
+//     if (!ctx.thenMethod) return
+//     ctx.value = ctx.thenExcute(this.value)
+//   }
+// }
 
-const reject = ctx => {
-  return value => {
-    ctx.value = value
-    ctx.status = 'rejected'
-    if (!ctx.cacthErr) throw ctx.value
-    ctx.value = this.cacthErr(value)
-  }
-}
+// const reject = ctx => {
+//   return value => {
+//     ctx.value = value
+//     ctx.status = 'rejected'
+//     if (!ctx.cacthErr) throw ctx.value
+//     ctx.value = this.cacthErr(value)
+//   }
+// }
 
 export default class BlackBox {
   constructor(fn) {
@@ -28,36 +28,30 @@ export default class BlackBox {
     this.status = 'pending'
     this.queue = []
 
-    // const resolve = value => {
-    //   this.value = value
-    //   this.status = 'resolved'
-    //   if (!this.thenMethod) return
-    //   this.value = this.thenMethod(this.value)
-    // }
+    const resolve = value => {
+      this.value = value
+      this.status = 'resolved'
+      if (!this.thenMethod) return
+      this.value = this.thenMethod(this.value)
+    }
 
-    // const reject = value => {
-    //   this.value = value
-    //   this.status = 'rejected'
-    //   if (!this.cacthErr) throw this.value
-    //   this.value = this.cacthErr(this.value)
-    // }
+    const reject = value => {
+      this.value = value
+      this.status = 'rejected'
+      if (!this.cacthErr) throw this.value
+      this.value = this.cacthErr(this.value)
+    }
 
-    fn(resolve(this), reject(this))
+    fn(resolve, reject)
   }
 
   then(onResolved, onRejected) {
     if (this.status === 'pending') {
-      this.thenExcute = onResolved
+      this.queue.push(onResolved)
       this.cacthErr = onRejected
-      return new this.constructor((resolve, reject) => {})
     }
-    return new this.constructor((resolve, reject) => {
-      this.status === 'resolved'
-        ? resolve(onResolved(this.value))
-        : onRejected
-        ? reject(onRejected(this.value))
-        : reject(this.value)
-    })
+    this.value = this.status === 'resolved' ? onResolved(this.value) : onRejected(this.value)
+    return this
   }
 
   catch(onCatched) {
